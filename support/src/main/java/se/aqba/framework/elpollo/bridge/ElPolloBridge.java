86ec2e71f31d3a6e771fd34c2129fbe11df8ad0c
Mathias Tillman <master.homer@gmail.com>
2015-03-13 12:54:45 +0100

    Fix framework not always overriding methods.

--- support/src/main/java/se/aqba/framework/elpollo/bridge/ElPolloBridge.java
+++ support/src/main/java/se/aqba/framework/elpollo/bridge/ElPolloBridge.java
@@ -2,11 +2,14 @@ package se.aqba.framework.elpollo.bridge;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 /**
  *  This class' methods will be overridden by the framework.
  */
 public class ElPolloBridge {
+    private static final String TAG = "ElPolloBridge";
+
     private static final String PREFERENCES_FILE = "elpollo_preferences";
 
     /**
@@ -14,7 +17,8 @@ public class ElPolloBridge {
      * @return Whether the framework is installed.
      */
     public static boolean isInstalled() {
-        return getVersionCode() != 0;
+        Log.w(TAG, "isInstalled was not overridden by the framework");
+        return false;
     }
 
     /**
@@ -22,6 +26,7 @@ public class ElPolloBridge {
      * @return The framework version name.
      */
     public static String getVersion() {
+        Log.w(TAG, "getVersion was not overridden by the framework");
         return "";
     }
 
@@ -29,13 +34,19 @@ public class ElPolloBridge {
      * Get the framework version code.
      * @return The framework version code.
      */
-    public static int getVersionCode() { return 0; }
+    public static int getVersionCode() {
+        Log.w(TAG, "getVersionCode was not overridden by the framework");
+        return 0;
+    }
 
     /**
      * Get the framework version codename.
      * @return The framework version codename.
      */
-    public static String getVersionCodename() { return ""; }
+    public static String getVersionCodename() {
+        Log.w(TAG, "getVersionCodename was not overridden by the framework");
+        return "";
+    }
 
     /**
      * Get the {@link android.content.SharedPreferences} of the current module.
@@ -50,5 +61,8 @@ public class ElPolloBridge {
      * Get the {@link android.content.SharedPreferences} of the system.
      * @return The preferences of the system.
      */
-    public static SharedPreferences getSystemPreferences() { return null; }
+    public static SharedPreferences getSystemPreferences() {
+        Log.w(TAG, "getSystemPreferences was not overridden by the framework");
+        return null;
+    }
 }
